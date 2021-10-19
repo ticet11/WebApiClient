7b3f5897ae064d90b546c0d73eb703bdf0687132
Brian Kozub <ticet11@gmail.com>
2021-10-19 01:32:05 -0600

    Can retrieve Departments list from database.

--- src/pages/Department.js
+++ src/pages/Department.js
@@ -1,7 +1,54 @@
 import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 
-export default class Department extends Component {
+interface dep {
+	departmentID: number;
+	departmentName: string;
+}
+export default class Department extends Component<{}, { deps: dep[] }> {
+	constructor(props) {
+		super(props);
+		this.state = { deps: [] };
+	}
+
+	componentDidMount() {
+		this.refreshList();
+	}
+
+	componentDidUpdate() {}
+
+	refreshList() {
+		fetch(process.env.REACT_APP_API + "department")
+			.then((response) => response.json())
+			.then((data) => {
+				this.setState({ deps: data.Value });
+				console.log(this.state.deps);
+			});
+	}
+
 	render() {
-		return <h1 className="mt-5 d-flex justify-content-left">Department Page, nice.</h1>;
+		const { deps } = this.state;
+		return (
+			<div className="">
+				<Table className="mt-4" striped bordered hover size="small">
+					<thead>
+						<tr>
+							<th>Department ID</th>
+							<th>Department Name</th>
+							<th>Options</th>
+						</tr>
+					</thead>
+					<tbody>
+						{deps.map((dep) => (
+							<tr key={`${dep.departmentID}`}>
+								<td>{dep.departmentID}</td>
+								<td>{dep.departmentName}</td>
+								<td>Edit / Delete</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			</div>
+		);
 	}
 }
