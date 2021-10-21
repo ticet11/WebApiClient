d4e53820b73cc975c8263251610368efa8ee0f12
Brian Kozub <ticet11@gmail.com>
2021-10-20 18:31:04 -0600

    Updated and refactored to use Typescript along with ESLint, more properly.

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Department from './pages/Department';
+import Employee from './pages/Employee';
+import Navigation from './components/Navigation';
+
+import './App.css';
+
+function App(): JSX.Element {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <h1 className="m-3 d-flex justify-content-center">React JS in .NET?</h1>
+      </div>
+
+      <Navigation />
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/departments" component={Department} />
+        <Route path="/employees" component={Employee} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
